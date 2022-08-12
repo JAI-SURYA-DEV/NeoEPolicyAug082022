874a14f1ab7d083c6a972aa384547ea8816efcc5
JAI-SURYA-WEB <89986800+JAI-SURYA-WEB@users.noreply.github.com>
2022-08-13 00:35:40 +0530

    icons added

--- src/app/Users/register/register.component.ts
+++ src/app/Users/register/register.component.ts
@@ -11,10 +11,12 @@ export class RegisterComponent implements OnInit {
   registerForm!: FormGroup;
   submitted = false;
   registerUsers: any = [];
+  currentDateTime: any;
   constructor(private _router: Router) {}
 
   ngOnInit(): void {
     this.registerForm = new FormGroup({
+      userId: new FormControl(this.currentDateTime),
       firstName: new FormControl(null, Validators.required),
       lastName: new FormControl(null, Validators.required),
       email: new FormControl(null, [Validators.required, Validators.email]),
@@ -46,6 +48,11 @@ export class RegisterComponent implements OnInit {
       console.log('registerForm.invalid');
       // alert('form not valid');
     } else {
+      this.generateSessionId();
+      this.registerForm.patchValue({
+        userId: this.currentDateTime,
+      });
+      console.log('Register Form Value', this.registerForm.value);
       console.log('registerForm.valid');
       console.log(this.registerUsers);
       if (this.registerUsers) {
@@ -58,11 +65,14 @@ export class RegisterComponent implements OnInit {
         );
       } else {
         console.log('registerUsers.null');
-        // localStorage.setItem(
-        //   'storedRregisterUsersData',
-        //   JSON.stringify(this.registerForm.value)
-        // );
+        this.registerUsers = [];
+        this.registerUsers.push(this.registerForm.value);
+        localStorage.setItem(
+          'storedRregisterUsersData',
+          JSON.stringify(this.registerUsers)
+        );
       }
+
       this._router.navigate(['/userList']);
       this.registerForm.reset();
     }
@@ -71,5 +81,18 @@ export class RegisterComponent implements OnInit {
   // go to userlist page
   goToUserList() {
     this._router.navigate(['userList']);
+  }
+
+  // generate current date time stamp
+  generateSessionId(): void {
+    let month = (new Date().getMonth() + 1).toString().padStart(2, '0');
+    let date = new Date().getDate().toString().padStart(2, '0');
+    let year = new Date().getFullYear();
+    let hours = new Date().getHours().toString().padStart(2, '0');
+    let minutes = new Date().getMinutes().toString().padStart(2, '0');
+    let seconds = new Date().getSeconds().toString().padStart(2, '0');
+    let milliseconds = new Date().getMilliseconds().toString().padStart(3, '0');
+    this.currentDateTime = `${date}${month}${year}${hours}${minutes}${seconds}${milliseconds}`;
+    // alert(this.currentDateTime);
   }
 }
